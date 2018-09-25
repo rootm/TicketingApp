03bda6dbc27a543e3324de445d019b814bbef633
muvindu-tharanga <muvindu.tharanga@my.sliit.lk>
2018-09-25 13:38:08 +0530

    initial commit

---
+++ app/src/main/java/lk/luna/muvindu/ticketingapp/MainScreen.kt
@@ -0,0 +1,12 @@
+package lk.luna.muvindu.ticketingapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
