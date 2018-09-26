0dbcd3950fa03c23c8be604bb4c7ee7e67f5b6f2
muvindu-tharanga <muvindu.tharanga@my.sliit.lk>
2018-09-26 10:47:42 +0530

    current journey details UI added

---
+++ app/src/main/java/lk/luna/muvindu/ticketingapp/JourneyCost.java
@@ -0,0 +1,13 @@
+package lk.luna.muvindu.ticketingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JourneyCost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journey_cost);
+    }
+}
---
+++ app/src/main/java/lk/luna/muvindu/ticketingapp/viewJourney.java
@@ -0,0 +1,13 @@
+package lk.luna.muvindu.ticketingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_journey);
+    }
+}
