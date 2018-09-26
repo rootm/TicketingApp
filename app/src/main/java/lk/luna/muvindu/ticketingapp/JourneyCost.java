62a454fd06c5ad094ac0ee6133731cb2c778d098
muvindu-tharanga <muvindu.tharanga@my.sliit.lk>
2018-09-26 15:20:46 +0530

    bus exit UI added

--- app/src/main/java/lk/luna/muvindu/ticketingapp/JourneyCost.java
+++ app/src/main/java/lk/luna/muvindu/ticketingapp/JourneyCost.java
@@ -1,5 +1,8 @@
 package lk.luna.muvindu.ticketingapp;
 
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class JourneyCost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journey_cost);
+
+        BusExit_Fragment fragment = BusExit_Fragment.newInstance("","");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+
+
     }
 }
